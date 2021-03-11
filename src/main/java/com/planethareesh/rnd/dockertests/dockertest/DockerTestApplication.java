be05eec2a56128f0b9f8f348b8d4e2edbf290cbc
trackhareesh <trackhareesh@gmail.com>
2021-03-11 20:53:37 +0530

    Initial commit

---
+++ src/main/java/com/planethareesh/rnd/dockertests/dockertest/DockerTestApplication.java
@@ -0,0 +1,21 @@
+package com.planethareesh.rnd.dockertests.dockertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DockerTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerTestApplication.class, args);
+	}
+	
+	@RequestMapping("/")
+	public String home() {
+		return "Hello World!!";
+	}
+
+}
---
+++ src/test/java/com/planethareesh/rnd/dockertests/dockertest/DockerTestApplicationTests.java
@@ -0,0 +1,18 @@
+package com.planethareesh.rnd.dockertests.dockertest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+		
+		// assertEquals(1, 2);
+	}
+
+
+}
